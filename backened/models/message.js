d577d90670e0d2968aad874dbc870f90bdc3c43e
ratheechakshita418 <ratheechakshita418@gmail.com>
2026-04-06 15:57:32 +0530

    Create message.js

---
+++ backened/models/message.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  recipientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  conversationId: String,
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Message', messageSchema);
